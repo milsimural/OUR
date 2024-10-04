931edad1982110cefa3836042f256660f9916b85
milsimural <108474929+milsimural@users.noreply.github.com>
2024-10-04 22:08:37 +0300

    last

--- sum.js
+++ sum.js
@@ -11,39 +11,47 @@ const rl = readline.createInterface({
 class View {
   constructor() {
     this.cat = 0;
-    this.chek = true;
+    this.check = true;
     this.points = 0;
     this.arrValue = [1, 2, 3];
   }
 
   run() {
-    function getN() {
-      const arrObj = Object.keys(controller.data);
-      return `Выбери одну из категорий:
+    const arrObj = Object.keys(controller.data);
+    const getN = `Выбери одну из категорий:
           ${this.arrValue[0]}. ${arrObj[0]}
           ${this.arrValue[1]}. ${arrObj[1]}
-          ${this.arrValue[2]}. ${arrObj[2]}`;
-    }
+          ${this.arrValue[2]}. ${arrObj[2]}\n\n`;
 
-    // rl.question(getN(), (answer) => {
-    //   this.cat = answer;
-    //   // rl.close();
-    // });
-    this.cat = prompt('Choose category! 1 2 3');
+    this.cat = prompt(getN);
 
-    while (this.chek) {
-      const question = controller.sendQuestion(this.cat);
-      if (typeof question === 'number') {
-        this.chek = false;
-        this.points = question;
-        console.log(`Кол-во очков: ${question}`);
-        break;
+    const runLoop = () => {
+      while (this.check) {
+        const question = controller.sendQuestion(this.cat);
+        console.log(`quest: ${question}`);
+
+        if (typeof question === 'number') {
+          this.check = false;
+          this.points = question;
+          console.log(`Кол-во очков: ${question}`);
+          rl.close();
+          break;
+          // return;
+        }
+
+        rl.question(`${Object.keys(question)}: `, (answer) => {
+          const reaction = controller.sendReaction(question, answer);
+          console.log(reaction);
+          runLoop();
+        });
+
+        // return;
       }
-      const answer = prompt(`Give answer: ${Object.keys(question)}`);
-      //   rl.question(`${Object.keys(question)}`, (answer) => {
-      const reaction = controller.sendReaction(question, answer);
-      console.log(reaction);
-    }
+
+      // rl.close();
+    };
+
+    runLoop();
   }
 }
 
@@ -51,11 +59,14 @@ class Controller {
   constructor(data) {
     this.data = data;
     this.questionNum = 0;
-    // this.score = 0;
   }
 
-  sendQuestion(category = 1) {
-    if (category === 1) {
+  sendQuestion(category) {
+    console.log(`ястребы${this.data.ястребы.length}`);
+    console.log(`еноты${this.data.еноты.length}`);
+    console.log(`выдры${this.data.выдры.length}`);
+    console.log(this.data.ястребы);
+    if (category == 1) {
       if (this.questionNum >= this.data.ястребы.length) {
         return model.getPoints();
       }
@@ -63,7 +74,7 @@ class Controller {
       this.questionNum++;
       return result;
     }
-    if (category === 2) {
+    if (category == 2) {
       if (this.questionNum >= this.data.еноты.length) {
         return model.getPoints();
       }
@@ -71,7 +82,7 @@ class Controller {
       this.questionNum++;
       return result;
     }
-    if (category === 3) {
+    if (category == 3) {
       if (this.questionNum >= this.data.выдры.length) {
         return model.getPoints();
       }
@@ -109,21 +120,43 @@ class Model {
     const otter = fs.readFileSync(`./topics/otter_flashcard_data.txt`, 'utf8');
     const raccoon = fs.readFileSync(`./topics/raccoon_flashcard_data.txt`, 'utf8');
 
-    const data = [[nighthawks], [otter], [raccoon]];
+    function prepareData(rawData) {
+      const rawArr = rawData.split('\r\n');
+      const objArr = [];
+      for (let i = 0; i < rawArr.lenght; i += 3) {
+        const obj = { [rawArr[i]]: rawArr[i + 1] };
+        console.log(obj);
+        objArr.push(obj);
+      }
+      console.log(`objArr: ${objArr}`);
+      return objArr;
+    }
 
-    const transformedData = {};
-    const animals = ['ястребы', 'выдры', 'еноты'];
+    const arrN = prepareData(nighthawks);
+    const arrO = prepareData(otter);
+    const arrR = prepareData(raccoon);
 
-    animals.forEach((animal, index) => {
-      const entries = data[index][0]
-        .trim()
-        .split('\n\n')
-        .map((entry) => {
-          const parts = entry.split('\n');
-          return { вопрос: parts[0], ответ: parts[1] || 'ответ' };
-        });
-      transformedData[animal] = entries;
-    });
+    const transformedData = {
+      ястребы: arrN,
+      выдры: arrO,
+      еноты: arrR,
+    };
+
+    // const data = [[nighthawks], [otter], [raccoon]];
+
+    // const transformedData = {};
+    // const animals = ['ястребы', 'выдры', 'еноты'];
+
+    // animals.forEach((animal, index) => {
+    //   const entries = data[index][0]
+    //     .trim()
+    //     .split('\n\n')
+    //     .map((entry) => {
+    //       const parts = entry.split('\n');
+    //       return { вопрос: parts[0], ответ: parts[1] || 'ответ' };
+    //     });
+    //   transformedData[animal] = entries;
+    // });
     this.data = transformedData;
   }
 }
