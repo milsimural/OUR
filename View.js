6c398390d499e1ac8d5cddd2395e4ca4b8cb6df1
milsimural <108474929+milsimural@users.noreply.github.com>
2024-10-04 14:45:59 +0300

    model-view

--- View.js
+++ View.js
@@ -1,16 +1,47 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 class View {
-    run() {
-        rl.question('Выберите категорию вопроса: ', (answer) => {
-            controller.(answer);
-        
-            rl.close();
-        });
+  constructor() {
+    this.cat = 0;
+    this.chek = true;
+    this.points = 0;
+    this.arrValue = [1, 2, 3];
+  }
+
+  run() {
+    function get() {
+      const arrObj = Object.keys(controller.data);
+      return `Выбери одну из категорий:   
+        ${this.arrValue[0]}. ${arrObj[0]}
+        ${this.arrValue[1]}. ${arrObj[1]}
+        ${this.arrValue[2]}. ${arrObj[2]}`;
     }
+
+    rl.question(get(), (answer) => {
+      this.cat = answer;
+      rl.close();
+    });
+
+    while (this.chek) {
+      const question = controller.sendQuestion(this.cat);
+      if (typeof question === 'number') {
+        this.chek = false;
+        this.points = question;
+        console.log(`Кол-во очков: ${question}`);
+        break;
+      }
+      rl.question(`${Object.keys(question)}`, (answer) => {
+        const reaction = controller.sendReaction(question, answer);
+        console.log(reaction);
+        rl.close;
+      });
+    }
+  }
 }
+
+module.export = View;
